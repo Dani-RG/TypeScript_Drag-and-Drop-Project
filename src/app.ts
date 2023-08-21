ebe73eba2c3830619bfea851eb5dbce1258a3493
Daniel-RG <21danielrondong@gmail.com>
2023-08-21 15:11:13 +0200

    124. Creating & using an AutoBind Decorator

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,24 @@
+//AUTOBIND DECORATOR:
+function autoBind(
+  _: any,
+  _2: string,
+  // ORIGINAL FIRST TWO PARAMETERS THAT WILL NOT BEING USED
+  // target: any,
+  // methodName: string,
+  descriptor: PropertyDescriptor
+) {
+  const originalMethod = descriptor.value;
+  const adjustedDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    }
+  };
+  return adjustedDescriptor;
+}
+
+//PROJECTINPUT CLASS:
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
@@ -18,24 +39,33 @@ class ProjectInput {
       true
     );
     this.element = importedNode.firstElementChild as HTMLFormElement;
-    this.element.id = 'user-input';
+    this.element.id = "user-input";
 
-    this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
-    this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
-    this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+    this.titleInputElement = this.element.querySelector(
+      "#title"
+    ) as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      "#description"
+    ) as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector(
+      "#people"
+    ) as HTMLInputElement;
 
     this.configure();
     this.attach();
   }
 
   //SET-UP:
+  @autoBind
   private submitHandler(event: Event) {
     event.preventDefault();
     console.log(this.titleInputElement.value);
   }
 
   private configure() {
-    this.element.addEventListener('submit', this.submitHandler.bind(this));
+    this.element.addEventListener("submit", this.submitHandler);
+    //WITHOUT DECORATOR:
+    // this.element.addEventListener('submit', this.submitHandler.bind(this));
   }
 
   private attach() {
