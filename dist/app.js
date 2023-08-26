9098586f083cafb557ce13295b7f95e5763f8eab
Daniel-RG <21danielrondong@gmail.com>
2023-08-26 14:55:26 +0200

    146. Using ES Modules

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,6 @@
 import { ProjectInput } from "./components/project-input.js";
 import { ProjectList } from "./components/project-list.js";
-// .js because we need to import them as if they were already compiled
-
 new ProjectInput();
 new ProjectList("active");
 new ProjectList("finished");
+//# sourceMappingURL=app.js.map
